fe14c57d5f48ea637e0bf3c66b11be3d2ff4b123
spborder <50718056+spborder@users.noreply.github.com>
2022-06-30 16:11:22 -0400

    Add files via upload

--- Accessory_Functions/staining-normalization-master/normalizeStaining.m
+++ Accessory_Functions/staining-normalization-master/normalizeStaining.m
@@ -72,30 +72,38 @@ I = double(I);
 
 I = reshape(I, [], 3);
 
-% calculate optical density
+% calculate optical density, size = size(I,1)*size(I,2)*size(I,3)
 OD = -log((I+1)/Io);
 
-% remove transparent pixels
+% remove transparent pixels, size = (depends on amount < beta), any rows
+% with one value (one OD channel) below beta are removed
 ODhat = OD(~any(OD < beta, 2), :);
 
-% calculate eigenvectors
+% calculate eigenvectors, size = [3,3]
 [V, ~] = eig(cov(ODhat));
 
 % project on the plane spanned by the eigenvectors corresponding to the two
-% largest eigenvalues
+% largest eigenvalues, size = [size(ODhat,1),2]
 That = ODhat*V(:,2:3);
 
-% find the min and max vectors and project back to OD space
+% find the min and max vectors and project back to OD space, size =
+% [size(ODhat,1),1], atan2 = four-quadrant inverse tangent
 phi = atan2(That(:,2), That(:,1));
 
+% This line finds the minimum and maximum values, adjusting alpha increases 
+% the tolerance in terms of resistance to outliers, size of each = 1
 minPhi = prctile(phi, alpha);
 maxPhi = prctile(phi, 100-alpha);
 
+% These lines find the projection of the minimum and maximum values and
+% projects them back into OD space, 
 vMin = V(:,2:3)*[cos(minPhi); sin(minPhi)];
 vMax = V(:,2:3)*[cos(maxPhi); sin(maxPhi)];
 
 % a heuristic to make the vector corresponding to hematoxylin first and the
 % one corresponding to eosin second
+% This is because the H stain is darker than the E stain in general and
+% contributes more strongly to the eigenvectors of the OD, size(HE) = [3,2]
 if vMin(1) > vMax(1)
     HE = [vMin vMax];
 else
@@ -105,19 +113,39 @@ end
 % rows correspond to channels (RGB), columns to OD values
 Y = reshape(OD, [], 3)';
 
-% determine concentrations of the individual stains
+% determine concentrations of the individual stains, '\' here represents
+% the solution to the linear equation of Y --> the OD values for each pixel
+% in the original RGB image, HE which are are the coefficients of H and E
+% in the current slide
 C = HE \ Y;
+%assignin('base','mixing',C)
+% C then equals the amount of each stain in each pixel
 
 % normalize stain concentrations
+% This line finds the maximum mixing value for each stain row
 maxC = prctile(C, 99, 2);
 
+% dividing each row by the maximum for that image
 C = bsxfun(@rdivide, C, maxC);
+% dividing the image normalized stuff by the maximum reference values
 C = bsxfun(@times, C, maxCRef);
 
+% Now C has a range of near 0 (for the minimum values) to maximum reference
+% values
 % recreate the image using reference mixing matrix
 Inorm = Io*exp(-HERef * C);
 Inorm = reshape(Inorm', h, w, 3);
 Inorm = uint8(Inorm);
+
+%     H = Io*exp(-HERef(:,1) * C(1,:));
+%     H = reshape(H', h, w, 3);
+%     H = uint8(H);
+% 
+%     E = Io*exp(-HERef(:,2) * C(2,:));
+%     E = reshape(E', h, w, 3);
+%     E = uint8(E);
+%     figure, subplot(121),imshow(H)
+%     subplot(122),imshow(E)
 
 if nargout > 1
     H = Io*exp(-HERef(:,1) * C(1,:));
