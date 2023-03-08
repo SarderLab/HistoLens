04aae6f47e9294acdb5082e4acc2b9eb24ee381b
spborder <50718056+spborder@users.noreply.github.com>
2023-03-08 09:53:21 -0500

    Add files via upload

--- Feature_Extraction_Functions/Catch_N_Release.m
+++ Feature_Extraction_Functions/Catch_N_Release.m
@@ -3,10 +3,18 @@ function Catch_N_Release(app,structure_name,feature_indices)
 
 % Getting all the slide names
 slide_names = app.Slide_Names;
-slide_names = slide_names(~contains(slide_names,'.xml'));
+slide_names = slide_names(~contains(slide_names,{'.xml','json'}));
 slide_path = app.Slide_Path;
 
-structure_idx = app.Structure_Names.(structure_name).Annotation_ID;
+%structure_idx = app.Structure_Names.(structure_name).Annotation_ID;
+structure_names = app.Structure_Names{:,1};
+structure_row = find(strcmp(structure_name,structure_names));
+structure_idx = app.Structure_Names{structure_row,2};
+structure_idx = strsplit(structure_idx{1},',');
+
+structure_idx = str2double(structure_idx);
+
+structure_idx_name = strcat('Structure_',num2str(structure_row));
 
 feat_filename = app.FeatSet_File;
 app.FeatureSetFilenameLabel.Text = strcat('Feature Set Filename: ',feat_filename);
@@ -33,7 +41,16 @@ for s = 1:length(slide_names)
 
     if contains(slide,'.')
         slide = strsplit(slide,'.');
+
+        wsi_ext = slide{end};
         slide = strjoin(slide(1:end-1),'_');
+    end
+
+    if ~strcmp(wsi_ext,'svs')
+        %display('Opening slide pointer')
+        slide_pointer = openslide_open(strcat(app.Slide_Path,filesep,slide_names{s}));
+    else
+        slide_pointer = [];
     end
 
     app.Slide_NormVals.(slide_idx_name).SlideName = slide;
@@ -45,30 +62,35 @@ for s = 1:length(slide_names)
     th.Position = [1 1 0];
     drawnow    
     
-    % Getting annotations for that slide
-    if contains(slide,'.svs')
-        file_name = strrep(slide,'.svs','.xml');
-    else
-        file_name = strrep(slide,'.ndpi','.xml');
-    end
+    if strcmp(app.Annotation_Format,'XML')
+        current_path = strcat(slide_path,filesep,slide,'.xml');
+        read_xml = xmlread(current_path);
+            
+        annotations = read_xml.getElementsByTagName('Annotation');
     
-    current_path = strcat(slide_path,filesep,file_name,'.xml');
-    read_xml = xmlread(current_path);
-        
-    annotations = read_xml.getElementsByTagName('Annotation');
-
-    % Uncomment this try-catch to account for any slides with 0 structures
-    % annotated
-    mpp = read_xml.getElementsByTagName('Annotations');
-    mpp = str2double(mpp.item(0).getAttribute('MicronsPerPixel'));
-
-    % When annotations are the result of network predictions, the MPP is
-    % not written to the xml file
-    if ~isnan(mpp)
-        app.MPP = mpp;
+        % Uncomment this try-catch to account for any slides with 0 structures
+        % annotated
+        mpp = read_xml.getElementsByTagName('Annotations');
+        mpp = str2double(mpp.item(0).getAttribute('MicronsPerPixel'));
+    
+        % When annotations are the result of network predictions, the MPP is
+        % not written to the xml file
+        if ~isnan(mpp)
+            app.MPP = mpp;
+        else
+            mpp = 0.2253;
+            app.MPP = mpp;
+        end
     else
-        mpp = 0.2253;
-        app.MPP = mpp;
+        current_path = strcat(slide_path,filesep,slide,'.json');
+        if ~isfile(current_path)
+            current_path = strrep(current_path,'.json','.geojson');
+            annotations = jsondecode(fileread(current_path));
+            annotations = annotations.features;
+        else
+            annotations = jsondecode(fileread(current_path));
+
+        end
     end
 
     slide_filepath = strcat(slide_path,filesep,slide,'.svs');
@@ -78,21 +100,46 @@ for s = 1:length(slide_names)
     structure_ID = slide_id;
     
     % Fix for multiple structure idxes for a single structure
-    reg_count = 1;
-    for st = 1:length(structure_idx)
-        structure_regions = annotations.item(structure_idx(st)-1);
-        if ~isempty(structure_regions)
-            regions = structure_regions.getElementsByTagName('Region');
-    
-            % Creating an array of all regions in the slide for more efficient
-            % parallel processing
-            for p = 0:regions.getLength-1
-                reg_array(reg_count) = regions.item(p);
-                reg_count = reg_count+1;
+    %reg_count = 1;
+    reg_array = [];
+    if strcmp(app.Annotation_Format,'XML')
+        for st = 1:length(structure_idx)
+            structure_regions = annotations.item(structure_idx(st)-1);
+            if ~isempty(structure_regions)
+                regions = structure_regions.getElementsByTagName('Region');
+        
+                % Creating an array of all regions in the slide for more efficient
+                % parallel processing
+                for p = 0:regions.getLength-1
+                    reg_array = [reg_array;regions.item(p)];
+                    %reg_count = reg_count+1;
+                end
+            end
+        end
+    else
+        for st = 1:length(annotations)
+            try
+                reg_array = [reg_array;annotations(st).coordinates];
+            catch
+                reg_array = [reg_array;annotations(st).geometry.coordinates];
             end
         end
     end
-        
+
+    reg_count = 0;
+    if strcmp(app.Annotation_Format,'XML')
+        for st = 1:length(structure_idx)
+            structure_regions = annotations.item(structure_idx(st)-1);
+            if ~isempty(structure_regions)
+                regions = structure_regions.getElementsByTagName('Region');
+                reg_count = reg_count+regions.getLength;
+            end
+        end
+    else
+        reg_count = reg_count+length(annotations);
+    end
+
+
     % Slide-level progress bar
     ph.XData = [0 ((s-1)/length(slide_names)) ((s-1)/length(slide_names)) 0];
     th.String = sprintf('%.0f%%',round(100*((s-1)/length(slide_names))));
@@ -105,26 +152,48 @@ for s = 1:length(slide_names)
     % parallelization, it doesn't like being passed 'app' as an argument.
     % So just need to extract the segmentation parameters before that
     seg_params = app.Seg_Params;
+    ann_format = app.Annotation_Format;
+
     try
-        parfor r = 1:length(reg_array)
+        parfor r = 1:reg_count
         %for r = 1:length(reg_array)
+            if strcmp(ann_format,'XML')
+                reg = reg_array(r);
+                verts = reg.getElementsByTagName('Vertex');
+                xy = zeros(verts.getLength-1,2);
+                for vi = 0:verts.getLength-1
+                    x = str2double(verts.item(vi).getAttribute('X'));
+                    y = str2double(verts.item(vi).getAttribute('Y'));
         
-            reg = reg_array(r);
-            verts = reg.getElementsByTagName('Vertex');
-            xy = zeros(verts.getLength-1,2);
-            for vi = 0:verts.getLength-1
-                x = str2double(verts.item(vi).getAttribute('X'));
-                y = str2double(verts.item(vi).getAttribute('Y'));
+                    xy(vi+1,:) = [x,y];
+                end
+        
+                mask_coords = zeros(size(xy));
+                bbox_coords = [min(xy(:,1))-100,max(xy(:,1))+100,min(xy(:,2))-100,max(xy(:,2))+100];
+                mask_coords(:,1) = xy(:,1)-bbox_coords(1);
+                mask_coords(:,2) = xy(:,2)-bbox_coords(3);
+            else
+                reg = reg_array(r);
+                
+                bbox_coords = [min(reg(:,1))-100,max(reg(:,1))+100,min(reg(:,2))-100,max(reg(:,2))+100];
+                mask_coords = zeros(size(reg));
+                mask_coords(:,1) = reg(:,1)-bbox_coords(1);
+                mask_coords(:,2) = reg(:,2)-bbox_coords(3);
+            end
+            
+            
+            if strcmp(wsi_ext,'svs')
+                raw_img = imread(slide_filepath,'Index',1,'PixelRegion',{bbox_coords(3:4),bbox_coords(1:2)});
+            else
+                %display('Non SVS File format')
+                min_x = bbox_coords(1);
+                min_y = bbox_coords(3);
+                range_x = bbox_coords(2)-bbox_coords(1);
+                range_y = bbox_coords(4)-bbox_coords(3);
     
-                xy(vi+1,:) = [x,y];
+                raw_img = openslide_read_region(slide_pointer,min_x,min_y,range_x,range_y);
             end
     
-            mask_coords = zeros(size(xy));
-            bbox_coords = [min(xy(:,1))-100,max(xy(:,1))+100,min(xy(:,2))-100,max(xy(:,2))+100];
-            mask_coords(:,1) = xy(:,1)-bbox_coords(1);
-            mask_coords(:,2) = xy(:,2)-bbox_coords(3);
-    
-            raw_img = imread(slide_filepath,'Index',1,'PixelRegion',{bbox_coords(3:4),bbox_coords(1:2)});
             mask = poly2mask(mask_coords(:,1),mask_coords(:,2),size(raw_img,1),size(raw_img,2));
     
             % Stain normalization
@@ -149,8 +218,15 @@ for s = 1:length(slide_names)
     
             feature_set = [feature_set;feat_row_combined];
         end
-        app.UITable.Data = [app.UITable.Data;{slide_id,num2str(regions.getLength)}];
+        
+        app.UITable.Data = [app.UITable.Data;{slide_id,num2str(reg_count)}];
+
         cell2csv(feat_filename,feature_set);
+    
+        if ~strcmp(wsi_ext,'svs')
+            %display('Closing slide pointer')
+            openslide_close(slide_pointer)
+        end
         
         
         clear reg_array
@@ -165,5 +241,5 @@ drawnow
 
 % Writing the features to the feature set file (get feature names)
 cell2csv(feat_filename,feature_set);
-app.Full_Feature_set.(structure_name) = readtable(feat_filename,'ReadVariableNames',true,'VariableNamingRule','preserve','Delimiter',',');
+app.Full_Feature_set.(structure_idx_name) = readtable(feat_filename,'ReadVariableNames',true,'VariableNamingRule','preserve','Delimiter',',');
 
