2bbaf3f21d5262cc9f5ac53fea08650115a53a26
spborder <50718056+spborder@users.noreply.github.com>
2023-03-08 09:42:42 -0500

    Add files via upload

---
+++ Accessory_Functions/openslide-matlab-master/openslide-wrapper.h
@@ -0,0 +1,5 @@
+#ifndef MATLAB_OPENSLIDE_WRAPPER_H
+#define MATLAB_OPENSLIDE_WRAPPER_H
+#define OPENSLIDE_SIMPLIFY_HEADERS
+#include "C:\Users\Sam\MATLAB Drive\HistoLens_Codes\Accessory_Functions\openslide-win64-20221217\include\openslide\openslide.h"
+#endif
