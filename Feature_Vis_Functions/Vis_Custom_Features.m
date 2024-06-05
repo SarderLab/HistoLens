fc8a2340cc159d463839ef88b78880b861754494
spborder <50718056+spborder@users.noreply.github.com>
2024-06-05 13:10:53 -0400

    Add files via upload

--- Feature_Vis_Functions/Vis_Custom_Features.m
+++ Feature_Vis_Functions/Vis_Custom_Features.m
@@ -1,70 +1,34 @@
 % --- Function where you can add visualizations for custom features
 % See documentation for definitions of different types of feature
 % visualizations for consistency
-function [all_feature_vis, feat_count] = Vis_Custom_Features(I,mask,composite,features_needed,all_feature_vis,vis_params)
-
-
-% Example features quantification silver stain content
-if any(ismember(features_needed,(448:461)))
-    
-    % stain stored as first channel in sub-compartment segmentation
-    silver_mask = composite(:,:,1);
-
-    % silver deposits greater than certain areas (pixel-wise)
-    area_breaks = (10:10:100);
-    silver_area_threshold_masks = cell(1,length(area_breaks));
-    for a=1:length(area_breaks)
+function [all_feature_vis, feat_count] = Vis_Custom_Features(I,mask,composite,features_needed,all_feature_vis,vis_params,feat_count)
+    % Example features quantification silver stain content
+    if any(ismember(features_needed,(449:460)))
         
-        intermediate_mask = bwareaopen(silver_mask,area_breaks(a));
-        silver_area_threshold_masks{a} = intermediate_mask;
+        % stain stored as first channel in sub-compartment segmentation
+        silver_mask = composite(:,:,1);
+    
+        % silver deposits greater than certain areas (pixel-wise)
+        area_breaks = (10:10:100);
+        silver_dist = bwdist(~silver_mask);
+        silver_area_threshold_masks = cell(1,length(area_breaks));
+        for a=1:length(area_breaks)
+            
+            intermediate_mask = silver_dist > area_breaks(a);
+            silver_area_threshold_masks{a} = intermediate_mask;
+        
+        end
+        
+        feat_vis = {silver_mask, silver_mask};
+        feat_vis = [feat_vis,silver_area_threshold_masks];
+    
+        overlap_num = length(find(ismember(features_needed,(449:460))));
+        feat_count = feat_count+overlap_num;
+    
+        all_feature_vis = insert_vis(features_needed,(449:460),feat_vis,all_feature_vis);
     
     end
-    
-    feat_vis = {silver_mask, silver_mask};
-    feat_vis = [feat_vis,silver_area_threshold_masks];
-
-    overlap_num = length(find(ismember(features_needed,(448:461))));
-    feat_count = feat_count+overlap_num;
-
-    all_feature_vis = insert_vis(features_needed,(448:461),feat_vis,all_feature_vis,)
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function all_feature_vis = insert_vis(feat_idxes, select_range, feat_subgroup, all_feature_vis)
