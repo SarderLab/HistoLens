5145b36d1e9b2c4771c9853e40c0784a0439d7fd
spborder <50718056+spborder@users.noreply.github.com>
2024-06-18 16:01:13 -0400

    Add files via upload

--- Startup_Functions/Add_Stain_Butts.m
+++ Startup_Functions/Add_Stain_Butts.m
@@ -4,17 +4,21 @@ function Add_Stain_Butts(app)
 stain_names = app.Stain_Names;
 delete(get(app.Stain_Peek,'Children'))
 
+grid_layouter = uigridlayout(app.Stain_Peek);
+grid_layouter.RowHeight = {'fit'};
 for i = 1:length(stain_names)
 
     %stain_field = strcat('Stain',num2str(i));
-    butt_position = [20+100*(i-1) 17 100 40];
+    %butt_position = [20+100*(i-1) 17 100 40];
 
-    stain_butt = uibutton(app.Stain_Peek,'push');
+    stain_butt = uibutton(grid_layouter,'push');
     stain_butt.Enable = 'off';
-    stain_butt.Position = butt_position;
+    %stain_butt.Position = butt_position;
+    stain_butt.Layout.Row = 1;
+    stain_butt.Layout.Column = i;
     stain_butt.Text = stain_names{i};
     stain_butt.FontWeight = 'bold';
-    stain_butt.ButtonPushedFcn = {@Comp_Peeking, i};
+    stain_butt.ButtonPushedFcn = {@Comp_Peeking, i, app};
     
 
 end
